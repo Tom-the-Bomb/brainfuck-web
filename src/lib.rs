5b344fa23865f998ff157139eb5b8558b5193627
Tom-the-Bomb <thello522@gmail.com>
2023-01-31 21:57:54 -0500

    finish bindings

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,15 @@ use std::io::Cursor;
 use wasm_bindgen::prelude::*;
 use brainfuck_exe::Brainfuck;
 
-#[must_use]
+
 #[wasm_bindgen]
-pub fn execute(code: &str, input: Option<String>) -> Result<JsValue, JsError> {
+pub fn execute(
+    code: &str,
+    input: Option<String>,
+    max_cell_value: Option<u32>,
+    memory_size: Option<usize>,
+    instructions_limit: Option<usize>,
+) -> Result<JsValue, JsError> {
     let buffer: Vec<u8> = Vec::new();
     let mut output = Cursor::new(buffer);
     let mut interp = Brainfuck::new(code)
@@ -12,8 +18,20 @@ pub fn execute(code: &str, input: Option<String>) -> Result<JsValue, JsError> {
 
     if let Some(input) = input {
         interp = interp.with_input(
-            Cursor::new(input.as_bytes().to_vec())
+            Cursor::new(input.into_bytes())
         );
+    }
+
+    if let Some(val) = max_cell_value {
+        interp = interp.with_max_value(val);
+    }
+
+    if let Some(mem) = memory_size {
+        interp = interp.with_mem_size(mem);
+    }
+
+    if let Some(limit) = instructions_limit {
+        interp = interp.with_instructions_limit(limit)
     }
 
     interp.execute()
@@ -21,10 +39,10 @@ pub fn execute(code: &str, input: Option<String>) -> Result<JsValue, JsError> {
             JsError::new(format!("{err}").as_str())
         )?;
 
-    Ok(String::from_utf8(output.get_ref().clone())
+    let output = String::from_utf8(output.get_ref().clone())
         .map_err(|err|
             JsError::new(format!("Something went wrong when decoding the output:\n{err}").as_str())
-        )?
-        .into()
-    )
+        )?;
+
+    Ok(output.into())
 }
